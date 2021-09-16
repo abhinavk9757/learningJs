687f1ebf868591a9300570d090a8ad2e92be9ab7
Abhinav Kumar <abhinavku@deloitte.com>
2021-09-16 07:15:37 +0530

    day1

---
+++ loops.js
@@ -0,0 +1,17 @@
+// for loop, while, do-while
+// forof, forEach
+// map, reduce, and filter
+
+
+// print 1-10
+// for(let a = 0;a<10;a++){
+//     console.log(a);
+// }
+
+const arr = ['qw','we', 'sd', 'sd', 'as'];
+// const len = arr.length;
+// for(let ctr = 0; ctr<len; ctr++){
+//     let currentElement = arr[ctr];
+//     console.log(currentElement);
+// }
+// arr.forEach((val) => { console.log(val) })
---
+++ types.js
@@ -0,0 +1,54 @@
+// Boolean
+// null
+// number
+// string
+// undefined
+// object
+
+// let a = 123;
+// let b = 'jhdfbhjd'
+
+
+// console.log(123 == '123')
+// console.log(123 === '123')
+
+// Objects are key/value pair
+
+// let a = {
+//     abc: 1,
+//     bcd: 23,
+//     'content-type': 345,
+// }
+
+// let b = {
+//     abc: 1,
+//     bcd: 23,
+//     'content-type': 345,
+// }
+
+// console.log(Object.keys(a))
+// let keys = Object.keys(a);
+
+// console.log(a[keys[0]])
+// console.log(a.keys[0])
+
+// console.log(Object.values(a));
+
+// let simpleVal = 12;
+// let simpleVal2 = 23;
+// simpleVal === simpleVal2;
+
+// let obj = {};
+// let obj2 = obj;
+// location1 => obj
+// location2 => obj2
+// console.log(obj == obj2)
+
+
+const obj = {
+    a1: 12,
+    a2: 22,
+    a3: 45
+}
+let key = 'a1';
+let val = obj[key];
