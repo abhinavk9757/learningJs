2e3c0d04a015af0621f47c78d70e137db184f3b6
Abhinav Kumar <abhinavku@deloitte.com>
2021-10-10 20:15:51 +0530

    day 12

---
+++ asyncSync.js
@@ -0,0 +1,45 @@
+// Almost everything except any external calls --> synchronous
+// api internet, file, timers
+
+// setTimeout, fetch
+
+// let a = 1;
+// let b= 2;
+// let c = a+b;
+
+// setTimeout(() => {
+//     console.log('this is an asynchronous call');
+// }, 1000)
+
+// const listStudent = fetch(url, { body: '', method: 'POST'});
+
+
+// console.log(listStudents);
+// let afterTimeout = 'jhdbjhjd';
+// afterTimeout = afterTimeout + 'jhdbjhbdjh';
+// console.log(afterTimeout);
+
+
+// callbacks
+// const async = (schoolName, class, callback) => {
+//     // do your logic
+
+//     callback();
+// }
+
+
+// const func1 = (callback) => {
+//     console.log('This is func 1');
+//     callback();
+// }
+
+
+// const afterFunc = () => {
+//     console.log("This is the log thats supposed to run run after func1")
+// }
+    
+// func1(afterFunc);
+
+// callbackHell
+// Promises => resolved, rejected
+
